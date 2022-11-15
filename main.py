08d5fb68d3c5c11738f9408fde9b4bb60992fb71
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-11-15 11:40:03 +0100

    Movements

--- main.py
+++ main.py
@@ -8,7 +8,27 @@ def move_forward():
     t.forward(10)
 
 
+def move_backward():
+    t.backward(10)
+
+
+def move_counter_clock():
+    t.left(10)
+
+
+def move_clock_wise():
+    t.right(10)
+
+
+def clear():
+    t.reset()
+
+
 s.listen()
-s.onkey(key="space", fun=move_forward)
+s.onkey(key="w", fun=move_forward)
+s.onkey(key="s", fun=move_backward)
+s.onkey(key="a", fun=move_counter_clock)
+s.onkey(key="d", fun=move_clock_wise)
+s.onkey(key="c", fun=clear)
 
 s.exitonclick()
